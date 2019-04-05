f801e43e0d2a88c674b853421bb0a2f85c0a63aa
Alonso Perez <nuevocorreo17@hotmail.com>
2019-04-04 21:16:17 -0500

    Terminamos el juego Ahorcado lineal en el Main de Program

--- TestProject/TestProject/Program.cs
+++ TestProject/TestProject/Program.cs
@@ -9,17 +9,49 @@ namespace TestProject {
         static void Main (string[] args) {
 
             string secretWord;
-            string showWord = "";
+            string shownWord = "";
             secretWord = Console.ReadLine ();
             Console.Clear ();
             for (int i = 0; i < secretWord.Length; i++) {
                 if (!(secretWord[i] == ' ')) {
-                    showWord += "*";
+                    shownWord += "*";
                 } else {
-                    showWord += " ";
+                    shownWord += " ";
                 }
             }
-            Console.WriteLine ("Your secret word is " + showWord);
+            Console.WriteLine ("Your secret word is " + shownWord);
+
+            while (!(shownWord == secretWord)) {
+                string charTry;
+                charTry = Console.ReadLine ();
+
+                while (charTry != secretWord || string.IsNullOrWhiteSpace(charTry) || charTry.Length > 1) {
+                    Console.Clear ();
+                    Console.WriteLine ("Your secret word is " + shownWord);
+                    Console.WriteLine ("Please input just one letter per try");
+                    charTry = Console.ReadLine ();
+                }
+
+                if (charTry == secretWord) {
+                    shownWord = charTry;
+                } else {
+
+                    if (secretWord.Contains (charTry)) {
+                        string tempWord = string.Empty;
+                        for (int i = 0; i < secretWord.Length; i++) {
+                            if (secretWord[i] == charTry[0]) {
+                                tempWord += secretWord[i];
+                            } else {
+                                tempWord += shownWord[i];
+                            }
+                        }
+                        shownWord = tempWord;
+                    }
+                }
+
+                Console.Clear ();
+                Console.WriteLine ("Your secret word is " + shownWord);
+            }
         }
     }
 }
