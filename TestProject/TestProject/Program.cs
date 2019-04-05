d5d94de8a4e712cabae43f3364b0a964a9001aff
Darthoul <hessenrod@gmail.com>
2019-04-04 22:09:41 -0500

    Ejemplo de funcionalidad, ver commit anterior para Tarea

--- TestProject/TestProject/Program.cs
+++ TestProject/TestProject/Program.cs
@@ -7,55 +7,8 @@ using System.Threading.Tasks;
 namespace TestProject {
     class Program {
         static void Main (string[] args) {
-            //------------------------INITIALIZATION--------------// 
-            string secretWord;
-            string shownWord = "";
-            secretWord = Console.ReadLine ();
-            Console.Clear ();
-            for (int i = 0; i < secretWord.Length; i++) {
-                if (!(secretWord[i] == ' ')) {
-                    shownWord += "*";
-                } else {
-                    shownWord += " ";
-                }
-            }
-            Console.WriteLine ("Your secret word is " + shownWord);
-            //----------------------------------------------------// 
 
-            //------------------------UPDATE-&-RENDER-------------// 
-            while (!(shownWord == secretWord)) {
-                string charTry;
-                charTry = Console.ReadLine ();
-
-                while (string.IsNullOrWhiteSpace(charTry) || charTry.Length > 1) {
-                    if (charTry == secretWord) { break; }
-                    Console.Clear ();
-                    Console.WriteLine ("Your secret word is " + shownWord);
-                    Console.WriteLine ("Please input just one letter per try");
-                    charTry = Console.ReadLine ();
-                }
-
-                if (charTry == secretWord) {
-                    shownWord = charTry;
-                } else {
-
-                    if (secretWord.Contains (charTry)) {
-                        string tempWord = string.Empty;
-                        for (int i = 0; i < secretWord.Length; i++) {
-                            if (secretWord[i] == charTry[0]) {
-                                tempWord += secretWord[i];
-                            } else {
-                                tempWord += shownWord[i];
-                            }
-                        }
-                        shownWord = tempWord;
-                    }
-                }
-
-                Console.Clear ();
-                Console.WriteLine ("Your secret word is " + shownWord);
-            }
-            //----------------------------------------------------// 
+            HangGame game = new HangGame ();
 
             //------------------------EXIT-PARAMETERS-------------// 
             Console.WriteLine ("Game finished...");
