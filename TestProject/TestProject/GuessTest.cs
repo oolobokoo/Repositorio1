961bc0caf39f0c59d724f8b73c90f50993cb7c9d
oolobokoo <klhey_j@hotmail.com>
2019-04-08 20:52:17 -0500

    Subiendo confirmacion del git ignore fucionado con visual-unity

--- TestProject/TestProject/GuessTest.cs
+++ TestProject/TestProject/GuessTest.cs
@@ -15,14 +15,7 @@ namespace TestProject {
             if (string.IsNullOrWhiteSpace (secretWord)) {
                 Console.WriteLine ("WARNING YOU MADE A GAME WITH EMPTY SECRETWORD");
                 return;
-            }
-            this.secretWord = secretWord;
-            for (int i = 0; i < secretWord.Length; i++) {
-                if (!(secretWord[i] == emptySpace)) {
-                    shownWord += symbol;
-                } else {
-                    shownWord += emptySpace;
-                }
+           
             }
         }
     }
